cb0051f3afad618abd7dcbc3678dc07737151abe
Vidhanvyrs <vidhanvyrs@gmail.com>
2026-01-07 01:54:39 +0530

    signin-signup page and UI changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { DotPattern } from "@/components/ui/dot-pattern"
 import { cn } from "@/lib/utils"
 import { Globe } from "@/components/ui/globe"
 import { Highlighter } from "@/components/ui/highlighter"
-import logo from '../assets/icon.svg'
+
 import {
     ScrollVelocityContainer,
     ScrollVelocityRow,
@@ -111,24 +111,7 @@ const WHO_USES_DATA = [
         description: "Automate community guidelines enforcement."
     }
 ]
-const FOOTER_LINKS = [
-    {
-        label: "PRODUCT",
-        links: ["SKILLS", "INTEGRATIONS", "PRICING", "LOGIN", "SIGN UP"]
-    },
-    {
-        label: "WHO USES COMMENTME",
-        links: ["SALES", "FINANCIAL ADVISORS", "EXECUTIVES", "RECRUITERS", "ASSISTANTS"]
-    },
-    {
-        label: "RESOURCES",
-        links: ["HELP", "USE CASES", "BLOG"]
-    },
-    {
-        label: "LEGAL",
-        links: ["PRIVACY", "TERMS", "SECURITY"]
-    }
-]
+
 
 const HomePage = () => {
     const [activeTab, setActiveTab] = useState(0)
@@ -394,43 +377,6 @@ const HomePage = () => {
                         </div>
                     </ScrollVelocityRow>
                 </ScrollVelocityContainer>
-            </div>
-
-            <div className="border-t border-white/10 px-2 py-2 text-[10px] tracking-[0.2em] font-medium text-gray-400 uppercase -mt-2">
-                <div className="flex flex-col space-y-12 m-10">
-                    {FOOTER_LINKS.map((row, idx) => (
-                        <div key={idx} className="flex flex-col md:flex-row md:items-center justify-between group hover:text-white transition-colors duration-500">
-                            <div className="mb-2 md:mb-0 text-white text-[12px] font-bold">{row.label}</div>
-                            <div className="flex flex-wrap gap-x-2 gap-y-1">
-                                {row.links.map((link, linkIdx) => (
-                                    <React.Fragment key={linkIdx}>
-                                        <a href="#" className="hover:text-[#F4D06F] transition-colors">{link}</a>
-                                        {linkIdx !== row.links.length - 1 && <span className="text-gray-700">/</span>}
-                                    </React.Fragment>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-                <div className="flex flex-col md:flex-row justify-between items-end border-white/10 p-4">
-                    <div className="flex items-center gap-4 mb-8 md:mb-0">
-                        <div className="text-2xl text-white font-normal lowercase tracking-tighter flex items-center gap-2">
-                            <img src={logo} alt="CommentMe Logo" className='rounded' /> Commentme
-                        </div>
-                        <span className="text-gray-600">© 2025 COMMENTME</span>
-                    </div>
-
-                    <div className="flex gap-4">
-                        <a href="#" className="hover:text-[#3e9cc4ff] transition-colors">LINKEDIN</a>
-                        <span className="text-gray-700">/</span>
-                        <a href="#" className="hover:text-[#3e9cc4ff] transition-colors">X</a>
-                        <span className="text-gray-700">/</span>
-                        <a href="#" className="hover:text-[#3e9cc4ff] transition-colors">YOUTUBE</a>
-                        <span className="text-gray-700">/</span>
-                        <a href="#" className="hover:text-[#3e9cc4ff] transition-colors">INSTAGRAM</a>
-                    </div>
-                </div>
             </div>
         </div>
     )
